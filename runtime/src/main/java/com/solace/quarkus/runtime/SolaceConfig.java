7fecd65902edf5dfa2de4f1c993eca9fcd9c031d
SravanThotakura05 <83568543+SravanThotakura05@users.noreply.github.com>
2024-01-24 12:23:47 +0530

    Fixed docs to include solace connection configuration

--- runtime/src/main/java/com/solace/quarkus/runtime/SolaceConfig.java
+++ runtime/src/main/java/com/solace/quarkus/runtime/SolaceConfig.java
@@ -22,7 +22,21 @@ public interface SolaceConfig {
     String vpn();
 
     /**
-     * Any extra parameters to pass to the Solace client
+     * Any extra parameters to pass to the Solace client.
+     * <br/>
+     * <br/>
+     * <br/>
+     * Refer to
+     * <a href=
+     * "https://docs.solace.com/API-Developer-Online-Ref-Documentation/pubsubplus-java/constant-values.html#com.solace.messaging.config.SolaceProperties.AuthenticationProperties.SCHEME">AuthenticationProperties</a>
+     * and
+     * <a href=
+     * "https://docs.solace.com/API-Developer-Online-Ref-Documentation/pubsubplus-java/constant-values.html#com.solace.messaging.config.SolaceProperties.TransportLayerProperties.COMPRESSION_LEVEL">TransportLayerProperties</a>
+     * for more configuration options
+     * <br/>
+     * <br/>
+     * <br/>
+     * Example: To configure compression `quarkus.solace.transport.compression-level`
      */
     @WithParentName
     Map<String, String> extra();
